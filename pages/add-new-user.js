8428d64caa0dbf534f76138371626d2247326882
rap.ev <raphaelmoa12@gmail.com>
2022-05-20 18:42:34 +0800

    Update users form and fixed classnames

--- pages/add-new-user.js
+++ pages/add-new-user.js
@@ -5,18 +5,29 @@ const addNewUser = () => {
     <div>
       <form className={styles.userForm}>
         <fieldset>
+          {/* <div className={styles.avatar}>
+            <img className={styles.avatarImg} src="" />
+            <input id="to-hide" type="file" onchange="previewFile()" />
+            <input
+              type="button"
+              className={styles.avatar}
+              value="Edit"
+              onClick="document.getElementById('to-hide').click()"
+            />
+          </div> */}
           <h2 className={styles.title}>Add User</h2>
           <h3 className={styles.subtitle}>
             Please provide complete information.
           </h3>
+          <input type="text" name="fullName" placeholder="Full Name" />
           <input type="text" name="email" placeholder="Email" />
           <input type="password" name="pass" placeholder="Password" />
           <input type="password" name="cpass" placeholder="Confirm Password" />
           <input
-            type="button"
-            name="next"
-            className="next action-button"
-            value="Next"
+            type="submit"
+            name="submit"
+            className={styles.submitButton}
+            value="Submit"
           />
         </fieldset>
         <fieldset>
